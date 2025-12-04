ec14a2c9883a98d8fc68598d664c2824325e919a
Alex <alexjf2912@gmail.com>
2025-12-04 18:39:54 +0100

    fix(adapter): Added the Component notation

--- src/main/java/com/example/similar_product/adapter/out/http/ProductHttpAdapter.java
+++ src/main/java/com/example/similar_product/adapter/out/http/ProductHttpAdapter.java
@@ -2,10 +2,12 @@ package com.example.similar_product.adapter.out.http;
 
 import com.example.similar_product.domain.model.ProductDetail;
 import com.example.similar_product.port.ProductPort;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@Component
 public class ProductHttpAdapter implements ProductPort {
     @Override
     public Mono<List<String>> getSimilarProductIds(String productId) {
